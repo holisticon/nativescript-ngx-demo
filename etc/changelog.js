5d50da09772451145dae8bc8a39fb1fbbe169993
Reinhardt, Martin <contact@martinreinhardt-online.de>
2017-05-23 06:51:13 +0200

    chore(build): Fix changelog error

--- etc/changelog.js
+++ etc/changelog.js
@@ -2,8 +2,12 @@
 //  e.g. changelog.js 42 -> uses 42 as build number
 var args = process.argv.slice(2),
   fs = require('fs'),
+  mkdirp = require('mkdirp'),
   exec = require('child_process').exec,
   buildNo = args[0] || process.env.BUILD_NUMBER || 1;
+
+const ANDROID_CHANGELOGS = __dirname + '/../fastlane/metadata/android/de-DE/changelogs/';
+const IOS_CHANGELOGS = __dirname + '/../fastlane/metadata/de-DE';
 
 console.log('buildNo: ' + buildNo);
 exec('git describe --tags --abbrev=0', function (err, tag) {
@@ -19,16 +23,29 @@ exec('git describe --tags --abbrev=0', function (err, tag) {
     }
     // cut to long text
     var changes = releaseNotes.replace(/"/, '').replace(/[a-z0-9A-Z]+/, '-').substring(0, 494) + ' ...';
+
     // PlayStore (Android)
-    fs.writeFile(__dirname + '/../fastlane/metadata/android/de-DE/changelogs/' + buildNo + '.txt', changes, function (err) {
+    mkdirp(ANDROID_CHANGELOGS, function (err) {
       if (err) {
         throw err;
+      } else {
+        fs.writeFile(ANDROID_CHANGELOGS + buildNo + '.txt', changes, function (err) {
+          if (err) {
+            throw err;
+          }
+        });
       }
     });
     // iTunes Connect (iOS)
-    fs.writeFile(__dirname + '/../fastlane/metadata/de-DE/release_notes.txt', changes, function (err) {
+    mkdirp(IOS_CHANGELOGS, function (err) {
       if (err) {
         throw err;
+      } else {
+        fs.writeFile(IOS_CHANGELOGS + '/release_notes.txt', changes, function (err) {
+          if (err) {
+            throw err;
+          }
+        });
       }
     });
   });
