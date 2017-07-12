ba03fe893e9ffc4d8c59639c677c5753160431e7
Reinhardt, Martin <contact@martinreinhardt-online.de>
2017-07-12 17:44:09 +0200

    fix(sass): Fix sass webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,11 @@ const nativescriptTarget = require("nativescript-dev-webpack/nativescript-target
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-
 const { AotPlugin } = require("@ngtools/webpack");
 
+const mainSheet = `./app.css`;
 const ENV = process.env.NODE_ENV || 'development';
 console.log('Active environment is: ', ENV);
-
-const mainSheet = `app.css`;
 
 module.exports = env => {
   const platform = getPlatform(env);
@@ -25,14 +23,15 @@ module.exports = env => {
     bundle: `./${nsWebpack.getEntryModule()}`,
 
     // Vendor entry with third-party libraries
-    vendor: `./vendor`,
+    vendor: `./vendor`
+
   };
 
   const rules = getRules();
   const plugins = getPlugins(platform, env);
   const extensions = getExtensions(platform);
 
-  return {
+  const config = {
     context: resolve("./app"),
     target: nativescriptTarget,
     entry,
@@ -49,7 +48,11 @@ module.exports = env => {
       modules: [
         "node_modules/tns-core-modules",
         "node_modules",
-      ]
+      ],
+
+      alias: {
+        '~': resolve("./app")
+      },
     },
     node: {
       // Disable node shims that conflict with NativeScript
@@ -61,6 +64,19 @@ module.exports = env => {
     module: { rules },
     plugins,
   };
+
+  if (env.snapshot) {
+    plugins.push(new nsWebpack.NativeScriptSnapshotPlugin({
+      chunk: "vendor",
+      projectRoot: __dirname,
+      webpackConfig: config,
+      targetArchs: ["arm", "arm64", "ia32"],
+      tnsJavaClassesOptions: { packages: ["tns-core-modules"] },
+      useLibs: false
+    }));
+  }
+
+  return config;
 };
 
 
@@ -78,9 +94,10 @@ function getRules() {
         "raw-loader",
       ]
     },
-    // Other CSS files get bundled using the raw loader
+    // CSS files get bundled using the raw loader
     {
       test: /\.css$/,
+      exclude: new RegExp(mainSheet),
       use: [
         "raw-loader",
       ]
@@ -88,12 +105,15 @@ function getRules() {
     // SASS support
     {
       test: /\.scss$/,
-      use: [
-        "raw-loader",
-        "resolve-url-loader",
-        "nativescript-dev-webpack/platform-css-loader",
-        "sass-loader",
-      ]
+      use: ExtractTextPlugin.extract({
+        use: [
+          "resolve-url-loader",
+          "nativescript-css-loader",
+          "nativescript-dev-webpack/platform-css-loader",
+          "sass-loader"
+        ]
+      })
+
     },
 
 
@@ -111,17 +131,16 @@ function getRules() {
 
 function getPlugins(platform, env) {
   let plugins = [
+
+    new ExtractTextPlugin(mainSheet),
+
     new BundleAnalyzerPlugin({
       analyzerMode: "static",
       openAnalyzer: false,
-      generateStatsFile: true,
-      reportFilename: join(__dirname, "report", `${platform}-report.html`),
-      statsFilename: join(__dirname, "report", `${platform}-stats.json`),
+      generateStatsFile: ENV === 'development',
+      reportFilename: join(__dirname, "target", "report", `${platform}-report.html`),
+      statsFilename: join(__dirname, "target", "report", `${platform}-stats.json`),
     }),
-
-    new ExtractTextPlugin(mainSheet),
-    new ExtractTextPlugin("platform.android.css"),
-    new ExtractTextPlugin("platform.ios.css"),
 
     // Vendor libs go to the vendor.js chunk
     new webpack.optimize.CommonsChunkPlugin({
@@ -159,9 +178,9 @@ function getPlugins(platform, env) {
 
     // Resolve .ios.css and .android.css component stylesheets, and .ios.html and .android component views
     new nsWebpack.UrlResolvePlugin({
-        platform: platform,
-        resolveStylesUrls: true,
-        resolveTemplateUrl: true
+      platform: platform,
+      resolveStylesUrls: true,
+      resolveTemplateUrl: true
     }),
 
   ];
